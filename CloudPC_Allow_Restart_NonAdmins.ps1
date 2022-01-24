8a9474870952d4a3bc1ee5781c890baf26738d6f
MortenPedholt <45897453+MortenPedholt@users.noreply.github.com>
2022-01-24 23:16:57 +0100

    Add files via upload

---
+++ CloudPC_Allow_Restart_NonAdmins.ps1
@@ -0,0 +1,15 @@
+﻿ $user = "Users"
+ $tmp = [System.IO.Path]::GetTempFileName()
+ secedit.exe /export /cfg $tmp
+ $settings = Get-Content -Path $tmp
+ $account = New-Object System.Security.Principal.NTAccount($user)
+ $sid = $account.Translate([System.Security.Principal.SecurityIdentifier])
+ for($i=0;$i -lt $settings.Count;$i++){
+     if($settings[$i] -match "SeShutdownPrivilege")
+     {
+         $settings[$i] += ",*$($sid.Value)"
+     }
+ }
+ $settings | Out-File $tmp
+ secedit.exe /configure /db secedit.sdb /cfg $tmp  /areas User_RIGHTS
+ Remove-Item -Path $tmp
